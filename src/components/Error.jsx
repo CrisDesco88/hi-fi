d6d1cac1606bc561f59eafc27f5086ebb53d4dc8
Usuario <cristiandescosido@gmail.com>
2022-10-10 20:08:29 -0300

    Song detail component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 const Error = () => (
 	<div className='w-full flex justify-center items-center'>
 		<h1 className='font-bold text-2xl text-white mt-2'>
-			Something went wrong. Please try again.
+			Algo salió mal. Intenta de nuevo por favor.
 		</h1>
 	</div>
 );
--- src/redux/sevices/shazamCore.js
+++ src/redux/sevices/shazamCore.js
@@ -1,20 +1,27 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-  export const shazamCoreApi = createApi({
-    reducerPath: 'shazamCoreApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-        prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', '4d10fcabc7mshe6b28c2381b61dap19262bjsnd952bc549420');
-            
-            return headers;
-        },
-    }),
-    endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => '/charts/world' }),
-    }),
-  });
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: headers => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '4d10fcabc7mshe6b28c2381b61dap19262bjsnd952bc549420'
+      )
 
-  export const {
-    useGetTopChartsQuery,
-  } = shazamCoreApi;
+      return headers
+    }
+  }),
+  endpoints: builder => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`
+    }),
+    getSongRelated: builder.query({
+      query: ({songid}) => `/tracks/related?track_id=${songid}`
+    }),
+  })
+})
+
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery } = shazamCoreApi
