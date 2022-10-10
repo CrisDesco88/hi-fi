fe4c3fcf341905adf0360101305f9520958376cc
Usuario <cristiandescosido@gmail.com>
2022-10-10 10:43:38 -0300

    Discover

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
+  
   // converts the time to format 0:00
-  const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;
+  const getTime = (time) =>{
+    const minutes = Math.floor(time / 60);
+    const seconds = (`0${Math.floor(time % 60)}`).slice(-2);
+    return `${minutes}:${seconds}`
+    };
 
   return (
     <div className="hidden sm:flex flex-row items-center">
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,27 +1,40 @@
-import { genres } from '../assets/constants';
-import Error from '../components/Error';
-import Loader from '../components/Loader';
-import SongCard from '../components/SongCard';
+import { useDispatch, useSelector } from "react-redux";
+import { genres } from "../assets/constants";
+import Error from "../components/Error";
+import Loader from "../components/Loader";
+import SongCard from "../components/SongCard";
 
-import { useGetTopChartsQuery } from '../redux/sevices/shazamCore';
+import { useGetTopChartsQuery } from "../redux/sevices/shazamCore";
 
 const Discover = () => {
-	const { data, isFetching, error } = useGetTopChartsQuery();
-	const genreTitle = 'Pop';
+	const dispatch = useDispatch();
+	const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-	if(isFetching) return <Loader title="Loading songs..."/>
-	if(error) return <Error/>;
+	const { data, isFetching, error } = useGetTopChartsQuery();
+	const genreTitle = "Pop";
+
+	if (isFetching) return <Loader title='Loading songs...' />;
+	if (error) return <Error />;
 
 	return (
-		<div className="flex flex-col">
-			<div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-				<h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
+		<div className='flex flex-col'>
+			<div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
+				<h2 className='font-bold text-3xl text-white text-left'>
+					Discover {genreTitle}
+				</h2>
 				<select
-					onChange={()=>{}}
-					value=""
+					onChange={() => {}}
+					value=''
 					className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
 				>
-					{genres.map(genre => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+					{genres.map((genre) => (
+						<option
+							key={genre.value}
+							value={genre.value}
+						>
+							{genre.title}
+						</option>
+					))}
 				</select>
 			</div>
 			<div className='flex flex-wrap sm:justify-start justify-center gap-8'>
@@ -30,11 +43,14 @@ const Discover = () => {
 						key={song.key}
 						song={song}
 						i={i}
+						isPlaying={isPlaying}
+						activeSong={activeSong}
+						data={data}
 					/>
 				))}
 			</div>
 		</div>
-	)
+	);
 };
 
 export default Discover;
