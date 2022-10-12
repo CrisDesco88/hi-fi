109961086c87d01c35a767e4e71d14ae256bad48
Usuario <cristiandescosido@gmail.com>
2022-10-12 16:45:48 -0300

    Settled serach bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { FiSearch } from "react-icons/fi";
+
 const Searchbar = () => (
-  <div>Loader</div>
+	<form
+		autocomplete='off'
+		className='p-2 text-gray-400 focus-within:text-gray-600'
+	>
+		<label
+			htmlFor='search-field'
+			className='sr-only'
+		>
+			Buscar canciones
+		</label>
+		<div className='flex flex-row justify-start items-center'>
+			<FiSearch className='w-5 h-5 ml-4' />
+			<input
+				type='text'
+				name='serch-field'
+				autoComplete='off'
+				id='search-field'
+				placeholder='Search'
+				value=''
+				onChange={() => {}}
+				className='flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4'
+			/>
+		</div>
+	</form>
 );
 
 export default Searchbar;
