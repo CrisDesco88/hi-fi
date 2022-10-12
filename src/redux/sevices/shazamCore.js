4f71edcda9032d5e6e9686462bcdc4114dee27ad
Usuario <cristiandescosido@gmail.com>
2022-10-12 16:45:10 -0300

    added getSongsByCountry query

--- src/redux/sevices/shazamCore.js
+++ src/redux/sevices/shazamCore.js
@@ -19,12 +19,21 @@ export const shazamCoreApi = createApi({
       query: ({ songid }) => `/tracks/details?track_id=${songid}`
     }),
     getSongRelated: builder.query({
-      query: ({songid}) => `/tracks/related?track_id=${songid}`
+      query: ({ songid }) => `/tracks/related?track_id=${songid}`
     }),
     getArtistDetails: builder.query({
-      query: (artistId) =>`/artists/details?artist_id=${artistId}`
+      query: artistId => `/artists/details?artist_id=${artistId}`
+    }),
+    getSongsByCountry: builder.query({
+      query: countryCode => `/charts/country?country_code=${countryCode}`
     })
   })
 })
 
-export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery } = shazamCoreApi
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery
+} = shazamCoreApi
